46326e8881aab56faefe75d5a97546cc50fe64bb
Devin Riegle <devin.riegle@joinroot.com>
2019-07-11 22:11:24 -0400

    Fix autoloading issue in production
    Identified here: https://github.com/deriegle/rails_starter_project/issues/1
    Fix: https://blog.bigbinary.com/2016/08/29/rails-5-disables-autoloading-after-booting-the-app-in-production.html

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module RailsAPIStarter
     config.load_defaults 5.1
 
     config.time_zone = "Eastern Time (US & Canada)"
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers

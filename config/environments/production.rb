3a70ebbbfabb9916085e328da5e6fd4554f0696d
Devin Riegle <riegledevin@gmail.com>
2021-05-01 06:50:28 -0400

    Updates to Rails 5 code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "whats-for-lunch-rails_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "rails_api_starter_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
